ab6a16acba72c48d330cfe009424432957770f96
samane26 <samane.teymurifar@gmail.com>
2022-04-25 21:23:24 +0430

    second

--- src/App.js
+++ src/App.js
@@ -2,19 +2,55 @@
 import React from 'react'
 import './App.scss'
 import Profile from './components/Profile'
+import Age from './components/Age'
+import Gender from './components/Gender'
+import Back from './components/Back'
+import { useState } from 'react'
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function App(){
 
-class App extends React.Component{
-  render(){
+  const getGender = (data)=>{
+    console.log("gender is",data);
+    return data;
+  };
+  let [gender,setGender]=useState(getGender());
+  console.log("can you see me?")
+  
     return(
-      <div className='wrapper'>
-        <Profile/>
-      </div>
+      <BrowserRouter>
+        <Routes path="/" element={<Profile/>}>
+          <Route index element={<Profile/>} />
+          <Route path="Age" element={<Age />} />
+          <Route path="Gender" element={<Gender func={getGender}/>} />
+          <Route path="Back" element={<Back />} />
+        </Routes>
+      </BrowserRouter>
+     
     )
-  }
+  
 }
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import NewIcon from './Icon';
+import "./style.scss";
 
-
-export default function Header(){
-    return(
-        <div>
-            <NewIcon/>
-            <p className='profileTitle'>پروفایل</p>
-        </div>
-    )
+function Header() {
+  return (
+    <div className="header">
+      <p> ! به داده وانه خوش آمدی</p>
+    </div>
+  );
 }
+
+export default Header;
